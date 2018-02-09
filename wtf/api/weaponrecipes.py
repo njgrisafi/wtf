cfc2814d04e66f09edb5bc379c62d74b483044d4
mrpudn <zach.mcauliffe@gmail.com>
2018-02-08 22:44:50 -0500

    Removed item recipes and armor

--- wtf/api/weaponrecipes.py
+++ wtf/api/weaponrecipes.py
@@ -1,37 +1,52 @@
 '''
 wtf.api.weaponrecipes
 
-Routes and functions for manipulating weapon recipes.
+Weapon recipes have the following properties:
+  * type: weapon type
+  * name: (customizable) weapon name
+  * description: (customizable) weapon description
+  * handedness: whether the weapon is one-handed or two-handed
+  * weight.center: weight interval center
+  * weight.radius: weight interval radius
+    > minimum weight = weight.center - weight.radius
+    > maximum weight = weight.center + weight.radius
+  * damage.min.center: minimum damage interval center
+  * damage.min.radius: minimum damage interval radius
+    > minimum damage.min = damage.min.center - damage.min.radius
+    > maximum damage.min = damage.min.center + damage.min.radius
+  * damage.max.center: maximum damage interval center
+  * damage.max.radius: maximum damage interval radius
+    > minimum damage.max = damage.max.center - damage.max.radius
+    > maximum damage.max = damage.max.center + damage.max.radius
 '''
-from wtf.api import itemrecipes
+
+
+WEAPON_TYPES = ['sword', 'axe', 'mace', 'dagger', 'bow']
 
 
 def create(**kwargs):
-    '''Create a weapon recipe.
-
-    Weapon recipes extend item recipes.
-
-    Weapon recipes have the following extended properties:
-        damage: how much damage the weapon can inflict
-            min: the minimum amount of damage the weapon can inflict
-                center: the center of the weapon's minimum damage interval
-                radius: the radius of the weapon's minimum damage interval
-            max: the maximum amount of damage the weapon can inflict
-                center: the center of the weapon's maximum damage interval
-                radius: the radius of the weapon's maximum damage interval
-    '''
-    recipe = itemrecipes.create(**kwargs)
+    '''Create a weapon recipe.'''
+    weight = kwargs.get('weight', {})
     damage = kwargs.get('damage', {})
     damage_min = damage.get('min', {})
     damage_max = damage.get('max', {})
-    recipe['damage'] = {
-        'min': {
-            'center': damage_min.get('center', 0),
-            'radius': damage_min.get('radius', 0)
+    return {
+        'type': kwargs.get('type'),
+        'name': kwargs.get('name'),
+        'description': kwargs.get('description'),
+        'handedness': kwargs.get('handedness', 1),
+        'weight': {
+            'center': weight.get('center', 0),
+            'radius': weight.get('radius', 0)
         },
-        'max': {
-            'center': damage_max.get('center', 0),
-            'radius': damage_max.get('radius', 0)
+        'damage': {
+            'min': {
+                'center': damage_min.get('center', 0),
+                'radius': damage_min.get('radius', 0)
+            },
+            'max': {
+                'center': damage_max.get('center', 0),
+                'radius': damage_max.get('radius', 0)
+            }
         }
     }
-    return recipe
--- wtf/api/weaponrecipes_test.py
+++ wtf/api/weaponrecipes_test.py
@@ -4,8 +4,10 @@ from wtf.api import weaponrecipes
 
 def test_weaponrecipes_create():
     expected = {
-        'name': 'foobar',
-        'description': 'foo bar baz',
+        'type': 'sword',
+        'name': 'Foo Sword',
+        'description': 'The mightiest sword in all the land.',
+        'handedness': 2,
         'weight': {'center': 12, 'radius': 3},
         'damage': {
             'min': {'center': 45, 'radius': 6},
@@ -13,8 +15,10 @@ def test_weaponrecipes_create():
         }
     }
     actual = weaponrecipes.create(
-        name='foobar',
-        description='foo bar baz',
+        type='sword',
+        name='Foo Sword',
+        description='The mightiest sword in all the land.',
+        handedness=2,
         weight=dict(center=12, radius=3),
         damage=dict(
             min=dict(center=45, radius=6),
@@ -26,8 +30,10 @@ def test_weaponrecipes_create():
 
 def test_weaponrecipes_create_defaults():
     expected = {
+        'type': None,
         'name': None,
         'description': None,
+        'handedness': 1,
         'weight': {'center': 0, 'radius': 0},
         'damage': {
             'min': {'center': 0, 'radius': 0},
